49d7a3e44b93b4bb6d83ed88da8eac98f81700a2
John Fawcett <jrf0110@gmail.com>
2015-05-25 19:58:15 -0500

    6 Global and requirejs builds

--- index.js
+++ index.js
@@ -2,21 +2,4 @@ module.exports        = require('./lib/base-view');
 module.exports.proxy  = require('./lib/server');
 module.exports.http   = require('./lib/http');
 
-if ( 'document' in global ){
-  document.addEventListener('DOMContentLoaded', function(){
-    var list = document.querySelectorAll('[data-gplaces]');
-
-    for ( var i = 0, el, options, target; i < list.length; i++ ){
-      el = list[ i ];
-      options = {};
-
-      target = el.getAttribute('data-target');
-
-      if ( target ){
-        options.target = document.querySelector( target );
-      }
-
-      module.exports( el, options );
-    }
-  });
-}
+require('./lib/register-browser-content')( module.exports );
